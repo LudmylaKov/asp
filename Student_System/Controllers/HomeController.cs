890b845042ede18c858797a29d56ff95f42d9919
Lucy <ludmylkakovalevych@gmail.com>
2017-10-06 19:17:58 +0300

    h

--- Student_System/Controllers/HomeController.cs
+++ Student_System/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Student_System.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ViewResult Index()
         {
             return View();
         }
---
+++ Student_System/Models/StudentModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Student_System.Models
+{
+    public class StudentModels
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
